edecf4b21ca0ca3207f196d619bab0c2e575b78c
SpaceWaffles <jackhartwig7@gmail.com>
2024-05-04 15:12:35 -0500

    init

---
+++ build/src/CMakeFiles/green-zone.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/green-zone.dir/main.cpp.o"
+  "CMakeFiles/green-zone.dir/main.cpp.o.d"
+  "green-zone"
+  "green-zone.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/green-zone.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/src/CMakeFiles/green-zone.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for green-zone.
+# This may be replaced when dependencies are built.
